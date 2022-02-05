0d55bc1a4a83fe2fdec9eb364dc25acbd77b552c
lisouch <lisa.elsorogy@hotmail.fr>
2022-02-05 16:46:36 +0100

    Prepare app for deploying

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,16 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.google.cloud.tools.appengine") version "2.4.2"
+}
+
+tasks.create("stage") {
+    dependsOn("installDist")
 }
 
 group = "noteapp.ktor.com"
-version = "0.0.1"
+version = "0.0.2"
 application {
     mainClass.set("noteapp.ktor.com.ApplicationKt")
 }
@@ -20,11 +26,22 @@ repositories {
     maven ("https://kotlin.bintray.com/kotlin-js-wrappers")
 }
 
+appengine {
+    stage {
+        setArtifact("build/libs/${project.name}-${project.version}-all.jar")
+    }
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "GCLOUD_CONFIG"
+    }
+}
+
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
+
 
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
