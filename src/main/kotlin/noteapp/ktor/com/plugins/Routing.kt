8df9355800738768fa163b2f8faf2e77fdb77e01
lisouch <lisa.elsorogy@hotmail.fr>
2022-02-05 17:27:41 +0100

    update

--- src/main/kotlin/noteapp/ktor/com/plugins/Routing.kt
+++ src/main/kotlin/noteapp/ktor/com/plugins/Routing.kt
@@ -6,8 +6,15 @@ import io.ktor.auth.*
 import io.ktor.auth.jwt.*
 import io.ktor.response.*
 import io.ktor.features.*
+import io.ktor.features.ContentTransformationException
 import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.request.*
 import noteapp.ktor.com.authentication.JwtConfig
+import noteapp.ktor.com.data.checkPasswordForEmail
+import noteapp.ktor.com.data.getUser
+import noteapp.ktor.com.data.requests.AccountRequest
+import noteapp.ktor.com.data.responses.SimpleResponse
 import noteapp.ktor.com.routes.loginRoute
 import noteapp.ktor.com.routes.noteRoutes
 import noteapp.ktor.com.routes.registerRoute
@@ -22,9 +29,31 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
-        loginRoute()
     }
     routing {
+            post("/login") {
+                val request = try {
+                    call.receive<AccountRequest>()
+                } catch(e: ContentTransformationException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                val isPasswordCorrect = checkPasswordForEmail(request.email, request.password)
+                val user = getUser(request.email)
+                if(isPasswordCorrect && user != null) {
+                    val token = jwtConfig.generateToken(JwtConfig.JwtUser(user.id, user.email))
+                    call.respond(token)
+                } else {
+                    call.respond(HttpStatusCode.OK, SimpleResponse(false, "The E-Mail or password is incorrect"))
+                }
+            }
+
+        authenticate {
+            get ("/me"){
+                val user = call.authentication.principal as JwtConfig.JwtUser
+                call.respond(user)
+            }
+        }
     }
 }
 
