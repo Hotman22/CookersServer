413af9ed5d91c5eb3e9f03d4af0f392cdcd36446
Hakim OTMANI (P) <hakim.otmani-ext@PARMPYLQ22F.ratpsmart.local>
2022-10-06 14:27:19 +0200

    remove auth from fetch recipe picture

--- src/main/kotlin/cookers/com/recipe/fetchrecippicture/FetchRecipePictureRoute.kt
+++ src/main/kotlin/cookers/com/recipe/fetchrecippicture/FetchRecipePictureRoute.kt
@@ -1,16 +1,13 @@
 package cookers.com.recipe.fetchrecippicture
 
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
 
 fun Route.fetchRecipePictureRoute() {
-    authenticate {
         get("/recipe/fetchrecipepicture") {
             val picturePath = call.request.queryParameters["picturePath"]
             call.respondFile(File("./uploads/$picturePath"))
-        }
     }
 }
